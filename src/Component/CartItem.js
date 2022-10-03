c52b523e457cd7fe8a03ec9bfa0c31b16e20596a
D-Savel <dad.savel@gmail.com>
2022-10-03 13:56:59 +0200

    modify CarItem component

--- src/Component/CartItem.js
+++ src/Component/CartItem.js
@@ -21,7 +21,7 @@ import {
 
 export const CartItem = (props) => {
   const { id, name, brand, imgUrl, price, offerPercent, qty, stock } = props
-  const [isMobile] = useMediaQuery('(max-width: 380px)')
+  const [isMobile] = useMediaQuery('(max-width: 420px)')
   const netPrice = itemPricing(price, offerPercent)
   const fontSizing = isMobile ? '0.8em' : '1.1em'
 
@@ -41,21 +41,21 @@ export const CartItem = (props) => {
           src={imgUrl}
           alt={name}
         />
-        <Box minW={isMobile ? '85px' : '300px'} display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+        <Box minW={!isMobile && '300px'} display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
           <Badge mb='1' fontSize='0.7em' px='2' borderRadius='md' variant='solid'>
             {brand.charAt(0).toUpperCase()}{brand.slice(1)}
           </Badge>
           <Text align='center' fontSize={fontSizing}>{name}</Text>
         </Box>
         <Text align='center' minW={!isMobile && '90px'} fontSize={fontSizing}> {formatAmountInEuro(netPrice)}</Text>
-        <NumberInput w={!isMobile && ('120px')} size='xs' min='0' max={stock > 10 ? '10' : stock} defaultValue={qty} onChange={handleUpdateItemQty}>
+        <NumberInput maxW='60px' size='sm' min='0' max={stock > 10 ? '10' : stock} defaultValue={qty} onChange={handleUpdateItemQty}>
           <NumberInputField />
           <NumberInputStepper>
             <NumberIncrementStepper />
             <NumberDecrementStepper />
           </NumberInputStepper>
         </NumberInput>
-        <Text align='center' minW={!isMobile && '90px'} fontSize={fontSizing} >{formatAmountInEuro(qty * netPrice)}</Text>
+        <Text align='center' minW={!isMobile && '95px'} fontSize={fontSizing} >{formatAmountInEuro(qty * netPrice)}</Text>
         <Button px='2' colorScheme='red' variant='solid' size='xs' onClick={handleDelCartItem}>x</Button>
       </HStack>
     </ListItem >
